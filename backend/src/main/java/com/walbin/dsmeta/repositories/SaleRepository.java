76c6ae1f58b547f5863bef35a9ad0dc12b2b34c6
walbine <walbin@gmail.com>
2022-07-13 22:32:42 -0300

    API teste

---
+++ backend/src/main/java/com/walbin/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.walbin.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.walbin.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
